e215805b33cc0fd3319d4396df675a6494e4d60f
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2023-09-26 21:55:18 -0300

    Exemplo de dom, função e outros

---
+++ externo.js
@@ -0,0 +1,5 @@
+//Comandos Javascript que serão executados na aplicação
+document.write("<strong>Linha escrita pelo JS externo</strong>");
+console.log("Arquivo JS externo executado");
+let nome = window.prompt("Qual seu nome?");
+window.alert("Olá "+nome);
