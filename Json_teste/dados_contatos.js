a88dae609ee7e15d0f23bf7794127ff46903af8d
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2023-11-14 21:58:14 -0300

    Implementação Busca e Estilo

--- Json_teste/dados_contatos.js
+++ Json_teste/dados_contatos.js
@@ -1,9 +1,10 @@
+var dados_contatos = null;
 const xmlhttp = new XMLHttpRequest();
 xmlhttp.onload = function () {
     //console.log(this.status);
     //console.log(this.statusText);
     //console.log(this.responseText);
-    const dados_contatos = JSON.parse(this.responseText);
+    dados_contatos = JSON.parse(this.responseText);
     listacontatos("contatos",dados_contatos);
     //console.log(JSON.stringify(dados_contatos));
 }
--- Json_teste/funcoes_contatos.js
+++ Json_teste/funcoes_contatos.js
@@ -1,9 +1,18 @@
-function listacontatos(destino, objcontatos) {    
+function listacontatos(destino, objcontatos, query = null) {
     let nrocontatos = objcontatos.contatos.length;
-    resultado = "<ul>";
-    for(i=0;i<nrocontatos;i++) {
-        resultado += "<li>Nome: "+objcontatos.contatos[i].nome+" - Cidade: "+objcontatos.contatos[i].cidade+"</li>";
+    resultado = "<table><tr><th>Nome</th><th>Cidade</th></tr>";
+    for (i = 0; i < nrocontatos; i++) {
+        //console.log(query);
+        //console.log(objcontatos.contatos[i].nome.search(query));
+        if (query != null) {
+            if (objcontatos.contatos[i].nome.toUpperCase().search(query.toUpperCase()) >= 0 ||
+                objcontatos.contatos[i].cidade.toUpperCase().search(query.toUpperCase()) >= 0) {
+                resultado += "<tr><td>" + objcontatos.contatos[i].nome + "</td><td>" + objcontatos.contatos[i].cidade + "</td></tr>";
+            }
+        } else {
+            resultado += "<tr><td>" + objcontatos.contatos[i].nome + "</td><td>" + objcontatos.contatos[i].cidade + "</td></tr>";
+        }
     }
-    resultado += "</ul>";
+    resultado += "</table>";
     document.getElementById(destino).innerHTML = resultado;
 }
